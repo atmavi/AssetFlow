ac9504c763d2848006e7dffbdc3ecef0d76109b9
atmavi <atmavi0001@gmail.com>
2026-05-10 07:21:49 +0800

    Added link to metric cards

--- frontend/src/components/MetricCard.jsx
+++ frontend/src/components/MetricCard.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function MetricCard({ title="", value=0 }) {
+function MetricCard({ title = "", value = 0, status="all" }) {
     return (
         <div className="rounded-lg bg-white p-5 shadow">
-            <p className="text-sm text-gray-500">{title}</p>
-            <p className="mt-2 text-3xl font-bold text-gray-900">{value}</p>
+            <Link to={`/assets?status=${status}`}>
+                <p className="text-sm text-gray-500">{title}</p>
+                <p className="mt-2 text-3xl font-bold text-gray-900">{value}</p>
+            </Link>
         </div>
     );
 }
