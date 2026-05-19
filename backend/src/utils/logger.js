9760188d9195f5f58169a664c5b02362fa853c6d
atmavi <atmavi0001@gmail.com>
2026-05-19 23:36:40 +0800

    add logger util

---
+++ backend/src/utils/logger.js
@@ -0,0 +1,38 @@
+const winston = require('winston');
+
+dotenv.config();
+
+// Define log levels and colors
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+};
+
+// Log everything in development, but only info and above in production
+const level = () => {
+  const env = process.env.NODE_ENV || 'development';
+  return env === 'development' ? 'debug' : 'info';
+};
+
+// Choose formatting: Clean text for local dev, raw JSON for production cloud logging
+const format = winston.format.combine(
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
+  winston.format.json()
+);
+
+// Link winston to the console
+const transports = [
+  new winston.transports.Console()
+];
+
+const logger = winston.createLogger({
+  level: level(),
+  levels,
+  format,
+  transports,
+});
+
+module.exports = logger;
